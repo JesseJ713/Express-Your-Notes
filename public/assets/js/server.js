eed90c2ce0c30c53508d92e9112f34b0890a7f08
JCJ <jesse.jackson.c@gmail.com>
2020-10-03 15:46:11 -0500

    Adding basic properties to server

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -2,3 +2,14 @@
 
 const express = require("express");
 const path = require("path");
+
+// Sets basic properties for Express server
+
+let app = express();
+
+let PORT = process.env.PORT || 3000;
+
+// Sets up data parsing capabilities
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
