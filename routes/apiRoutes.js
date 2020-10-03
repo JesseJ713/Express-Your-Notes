0497fd723145e96dfa878630f17f70311a295806
JCJ <jesse.jackson.c@gmail.com>
2020-10-03 17:07:26 -0500

    Adds function that will add unique id to entries

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,3 +2,16 @@
 const { urlencoded } = require("body-parser");
 const fs = require("fs");
 let userData = readNotes();
+
+function readNotes() {
+  let data = fs.readFileSync("../db/db.json", "utf-8");
+
+  let userInput = JSON.parse(data);
+
+  // Giving each entry its own unique ID
+  for (let i = 0; i < userInput.length; i++) {
+    userInput[i].id = "" + i;
+  }
+
+  return userInput;
+}
