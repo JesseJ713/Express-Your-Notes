8d86ca07c419b940aa6beef474eec97cfa847473
JCJ <jesse.jackson.c@gmail.com>
2020-10-03 16:08:25 -0500

    Moved server file out of js directory. Added route dependencies

--- server.js
+++ server.js
@@ -4,16 +4,18 @@ const express = require("express");
 const path = require("path");
 
 // Sets basic properties for Express server
-
 let app = express();
 
 let PORT = process.env.PORT || 3000;
 
 // Sets up data parsing capabilities
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Pointing Express to `public` directory
-
 app.use(express.static("public"));
+
+// Establishes routes that will create a "map" for our server files
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
