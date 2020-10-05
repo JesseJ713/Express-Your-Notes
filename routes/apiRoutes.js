e5f3ebc3eef61d3e5b06f457e65e6a2b2664566b
JCJ <jesse.jackson.c@gmail.com>
2020-10-05 13:33:50 -0500

    Successful server deployment. Still need to work on functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,46 +1,48 @@
 // Dependencies
 const { urlencoded } = require("body-parser");
 const fs = require("fs");
-let noteData = readNotes();
+const noteObjects = require("../db/db");
+// let noteData = readNotes();
 
-function readNotes() {
-  let data = fs.readFileSync("../db/db.json", "utf8");
+// function readNotes() {
+//   let data = fs.readFileSync("../db/db.js", "utf8");
 
-  let userInput = JSON.parse(data);
+//   let userInput = JSON.parse(data);
 
-  // Giving each entry its own unique ID
-  for (let i = 0; i < userInput.length; i++) {
-    userInput[i].id = "" + i;
-  }
+//   // Giving each entry its own unique ID
+//   for (let i = 0; i < userInput.length; i++) {
+//     userInput[i].id = "" + i;
+//   }
 
-  return userInput;
-}
+//   return userInput;
+// }
 
 // GET, POST, DELETE requests
 module.exports = function (app) {
   app.get("/api/notes", function (req, res) {
-    noteData = readNotes();
-    res.JSON(noteData);
+    // noteData = readNotes();
+    res.JSON(noteObjects);
   });
 
   app.post("/api/notes", function (req, res) {
-    noteData.push(req.body);
-    fs.writeFileSync("../db/db.json", JSON.stringify(noteData), "utf8");
+    noteObjects.push(req.body);
+    // fs.writeFileSync("../db/db.js", JSON.stringify(noteData), "utf8");
     res.JSON(true);
   });
 
   app.delete("/api/notes/:id", function (req, res) {
-    const noteId = req.params.id;
+    // const noteId = req.params.id;
+    // let note = noteData.filter((note) => {
+    //   return note.id === noteId;
+    // })[0];
+    // const index = noteData.indexOf(note);
+    // noteData.splice(index, 1);
+    // fs.writeFileSync("../db/db.js", JSON.stringify(noteData), "utf8");
+    // res.JSON("Note has been deleted");
+    noteObjects.length = 0;
 
-    let note = noteData.filter((note) => {
-      return note.id === noteId;
-    })[0];
-
-    const index = noteData.indexOf(note);
-
-    noteData.splice(index, 1);
-
-    fs.writeFileSync("../db/db.json", JSON.stringify(noteData), "utf8");
-    res.JSON("Note has been deleted");
+    res.json({ ok: true });
   });
 };
+
+console.log(noteObjects);
