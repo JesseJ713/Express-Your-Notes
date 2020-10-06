8753f1f8bae20f63e070a9cd54f58a032afcaac9
JCJ <jesse.jackson.c@gmail.com>
2020-10-06 00:02:49 -0500

    Removing comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,43 +2,19 @@
 const { urlencoded } = require("body-parser");
 const fs = require("fs");
 const noteObjects = require("../db/db");
-// let noteData = readNotes();
-
-// function readNotes() {
-//   let data = fs.readFileSync("../db/db.js", "utf8");
-
-//   let userInput = JSON.parse(data);
-
-//   // Giving each entry its own unique ID
-//   for (let i = 0; i < userInput.length; i++) {
-//     userInput[i].id = "" + i;
-//   }
-
-//   return userInput;
-// }
 
 // GET, POST, DELETE requests
 module.exports = function (app) {
   app.get("/api/notes", function (req, res) {
-    // noteData = readNotes();
     res.json(noteObjects);
   });
 
   app.post("/api/notes", function (req, res) {
     noteObjects.push(req.body);
-    // fs.writeFileSync("../db/db.js", JSON.stringify(noteData), "utf8");
     res.json(true);
   });
 
   app.delete("/api/notes/:id", function (req, res) {
-    // const noteId = req.params.id;
-    // let note = noteObjects.filter((note) => {
-    //   return note.id === noteId;
-    // })[0];
-    // const index = noteObjects.indexOf(note);
-    // noteObjects.splice(index, 1);
-    // fs.writeFileSync("../db/db.js", JSON.stringify(noteData), "utf8");
-    // res.JSON("Note has been deleted");
     noteObjects.length = 0;
     res.json({ ok: true });
   });
